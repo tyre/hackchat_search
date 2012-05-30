584a0a39fa98bbb0f382fded0f36f62a8e52c50a
Chris Maddox <tyre77@gmail.com>
2012-05-29 20:13:12 -0400

    initial noir commit

---
+++ src/search/views/welcome.clj
@@ -0,0 +1,9 @@
+(ns search.views.welcome
+  (:require [search.views.common :as common]
+            [noir.content.getting-started])
+  (:use [noir.core :only [defpage]]
+        [hiccup.core :only [html]]))
+
+(defpage "/welcome" []
+         (common/layout
+           [:p "Welcome to search"]))
